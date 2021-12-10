a4be032d655471779edec0a11039d74aed9d21a0
octolili <lihmcf@gmail.com>
2021-12-09 17:30:07 -0700

    my first time database

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
 app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.set('view engine', 'ejs')
+
+const timesDataBase = {
+  'Bob': '57.85',
+  'Jon': '47.88',
+  'Bill': '44.76'
+}
+
 app.get('/', function (req, res) {
-  res.send('Hello World! Awesome')
+  const TemplateVars= {
+    skaters: timesDataBase
+  }
+  res.render('index',
+  TemplateVars)
+})
+
+app.post('/times/new', function (req, res) {
+  console.log('Times Input', req.body)
+  timesDataBase[req.body.FirstName] = req.body.Time
+  console.log(timesDataBase)
+  res.redirect('/')
 })
  
 app.listen(PORT, function(){
   console.log(`server running listening on: ${PORT}`)
+  console.log(timesDataBase)
 });
